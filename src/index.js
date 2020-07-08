71b124afedf1a209d9076af0e894e6c91d540a00
Tumaho <obadatumah19@gmail.com>
2020-07-09 02:10:14 +0300

    auth login

--- src/index.js
+++ src/index.js
@@ -3,14 +3,14 @@ import ReactDOM from 'react-dom';
 
 import ToDo from './components/todo/todo'
 
-// import App from './app.js';
+import App from './app.js';
 
 
-// class Main extends React.Component {
-//   render() {
-//     return <App />;
-//   }
-// }
+class Main extends React.Component {
+  render() {
+    return <App />;
+  }
+}
 
 const rootElement = document.getElementById('root');
-ReactDOM.render(<ToDo />, rootElement);
+ReactDOM.render(<Main />, rootElement);
